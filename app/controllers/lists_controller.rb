ef336584efa8576e6c35f26fa511a3e37003fc50
Getro Naissance <getro89@gmail.com>
2017-02-13 21:33:13 -0800

    it will add the index, show, new and create actions along with their respective views

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,17 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+    render :index
+  end
+
+  def show
+    @list = List.find(params[:id])
+    render :show
+  end
+
+  def new
+    @list = List.new
+    render :new
+  end
+
+end
